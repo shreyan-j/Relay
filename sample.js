64890bf85279c6e6a378c74006da91d4bf7ee6c8
shreyan-j <jshreyan20@gmail.com>
2026-08-29 15:09:36 +0530

    v.1.0.3

---
+++ sample.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/about-me', (req, res) => {
+  res.sendFile(path.join(__dirname, 'aboutme.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
